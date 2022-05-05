fd2209f2cd8747d6cb600964af4e01e6552c9b56
MikhaylovVladislav <79971836+MikhaylovVladislav@users.noreply.github.com>
2022-05-05 19:12:29 +0500

    Настроил параметры подключения к бд

--- connectToDB.php
+++ connectToDB.php
@@ -1,11 +1,9 @@
 <?php
 define ("HOST", "localhost");
-define ("USER", "root");
-define ("PASS", "");
-define ("DB", "review");
+define ("USER", "f0593353_root");
+define ("PASS", "12345");
+define ("DB", "f0593353_students");
  
  $linkmy = @mysqli_connect(HOST, USER, PASS, DB) or die ('Не получилось из-за @mysqli_connect :(');
  mysqli_select_db($linkmy, "f0593353_students") or die("Нет такой таблицы!");
-
-
  ?>
